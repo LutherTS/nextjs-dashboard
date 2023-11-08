86835dabd055579d4ba83dbb8079faacbb8400e7
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-11-08 14:09:28 +0100

    Streaming now function at the component level.

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,7 +1,7 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
+// import { Revenue } from '@/app/lib/definitions';
 import { fetchRevenue } from '@/app/lib/data';
 
 // This component is representational only.
@@ -20,7 +20,7 @@ export default async function RevenueChart({
 
 export default async function RevenueChart() {
   const revenue = await fetchRevenue();
-  
+
   const chartHeight = 350;
   // NOTE: comment in this code when you get to this point in the course
 
