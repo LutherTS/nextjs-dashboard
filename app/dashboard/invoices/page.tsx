69f56b505f981b7aba6478952dde16f8b2ef78ff
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-11-08 20:11:46 +0100

    -mt-2 it is for dashboard pages h1s.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -22,7 +22,8 @@ export default async function Page({
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+        {/* Added -mt-2 here too. */}
+        <h1 className={`${lusitana.className} text-2xl -mt-2`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
