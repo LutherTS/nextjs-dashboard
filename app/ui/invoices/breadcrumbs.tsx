d68ecdae3ed32d3ae4dad4e71af003d111c89a0b
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-11-09 14:42:47 +0100

    Cursor default on breadcrumbs.

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -25,9 +25,15 @@ export default function Breadcrumbs({
               breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
             )}
           >
-            <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+            <Link className={clsx(
+              breadcrumb.active ? 'cursor-default' : null,
+            )} 
+            href={breadcrumb.href}
+            >
+              {breadcrumb.label}
+            </Link>
             {index < breadcrumbs.length - 1 ? (
-              <span className="mx-3 inline-block">/</span>
+              <span className="mx-3 inline-block cursor-default">/</span>
             ) : null}
           </li>
         ))}
