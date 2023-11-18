cf498397c68e7258382c4e89938bcc676ae29913
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-11-18 08:02:00 +0100

    Let's try upload then.

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,9 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ["/((?!api|_next/static|_next/image|icon.jpg|.png).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|icon.jpg|.png).*)",
+    "/avatar/upload",
+  ],
   // Now with added icon.jpg.
 };
