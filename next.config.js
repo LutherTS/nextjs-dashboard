e27983af6c22eda35fc4fc47700b070b030b476a
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-11-18 08:44:31 +0100

    Made blobs compatible with next/image.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ts8v2xbiemab19zt.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**/*",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
